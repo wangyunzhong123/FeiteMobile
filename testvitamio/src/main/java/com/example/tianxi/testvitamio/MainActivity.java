93af8dd9c85b6ed3eeade92537dc45c05061c945
wangyunzhong123 <1337478765@qq.com>
2015-10-30 15:34:10 +0800

    初次提交，实现rtmp播放

---
+++ testvitamio/src/main/java/com/example/tianxi/testvitamio/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.tianxi.testvitamio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "MainActivity";
+    private String path;
+    //private HashMap<String, String> options;
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!LibsChecker.checkVitamioLibs(this))
+            return;
+        setContentView(R.layout.activity_main);
+        mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
+        path = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+            /*options = new HashMap<>();
+            options.put("rtmp_playpath", "");
+            options.put("rtmp_swfurl", "");
+            options.put("rtmp_live", "1");
+            options.put("rtmp_pageurl", "");*/
+        mVideoView.setVideoPath(path);
+        //mVideoView.setVideoURI(Uri.parse(path), options);
+        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.requestFocus();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
+        });
+    }
+}
